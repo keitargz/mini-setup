2d153f8168a8074e918e5efdfcd229900f4e4620
keitargz <keita750Ti@gmail.com>
2024-10-06 13:30:41 +0900

    Update config.fish

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,4 +2,7 @@ if status is-interactive
 	set fish_greeting
 	source /usr/share/powerline/bindings/fish/powerline-setup.fish
 	powerline-setup
+	if test (tty) = "/dev/tty1"
+		startx &> /dev/null
+	end
 end
